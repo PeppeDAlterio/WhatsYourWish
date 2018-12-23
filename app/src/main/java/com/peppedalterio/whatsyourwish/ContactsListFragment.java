fa6c8e562581d89cd96461a9f759d92a1035e895
Giuseppe D'Alterio <dalterio2@gmail.com>
2018-12-23 15:20:33 +0100

    Tutorial Contacts List from Android devs

--- app/src/main/java/com/peppedalterio/whatsyourwish/ContactsListFragment.java
+++ app/src/main/java/com/peppedalterio/whatsyourwish/ContactsListFragment.java
@@ -1,33 +1,217 @@
 package com.peppedalterio.whatsyourwish;
 
+import android.annotation.SuppressLint;
+import android.database.Cursor;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.LoaderManager;
+import android.support.v4.content.CursorLoader;
+import android.support.v4.content.Loader;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.Button;
+import android.widget.ListView;
+import android.widget.SimpleCursorAdapter;
 import android.widget.Toast;
+import android.provider.ContactsContract;
 
-public class ContactsListFragment extends Fragment {
+public class ContactsListFragment extends Fragment implements
+        LoaderManager.LoaderCallbacks<Cursor>,
+        AdapterView.OnItemClickListener {
+
 
     private static final String TAG = "ContactsListFragment";
 
-    private Button bttst;
+    /*
+        Define a constant that contains the columns you want to return from your query.
+        Each item in the ListView displays the contact's display name, which contains the
+        main form of the contact's name.
+         */
+    @SuppressLint("InlinedApi")
+    private static final String[] PROJECTION =
+    {
+            ContactsContract.Contacts._ID,
+            ContactsContract.Contacts.LOOKUP_KEY,
+            Build.VERSION.SDK_INT
+                    >= Build.VERSION_CODES.HONEYCOMB ?
+                    ContactsContract.Contacts.DISPLAY_NAME_PRIMARY :
+                    ContactsContract.Contacts.DISPLAY_NAME
+
+    };
+    /*
+    To get data from an individual column in a Cursor, you need the column's index
+    within the Cursor. You can define constants for the indexes of the Cursor columns,
+    because the indexes are the same as the order of the column names in your projection.
+     */
+    // The column index for the _ID column
+    private static final int CONTACT_ID_INDEX = 0;
+    // The column index for the CONTACT_KEY column
+    private static final int CONTACT_KEY_INDEX = 1;
+
+
+    /*
+    To specify the data you want, create a combination of text expressions and variables
+    that tell the provider the data columns to search and the values to find.
+     */
+    // Defines the text expression
+    @SuppressLint("InlinedApi")
+    private static final String SELECTION =
+            Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB ?
+                    ContactsContract.Contacts.DISPLAY_NAME_PRIMARY + " LIKE ?" :
+                    ContactsContract.Contacts.DISPLAY_NAME + " LIKE ?";
+    // Defines a variable for the search string
+    private String mSearchString;
+    // Defines the array to hold values that replace the ?
+    private String[] mSelectionArgs = { mSearchString };
+
+
+
+
+
+
+
+    /*
+     * Defines an array that contains column names to move from
+     * the Cursor to the ListView.
+     * CursorLoader is used to retrieve data from the provider (ContactsProvider)
+     */
+    @SuppressLint("InlinedApi")
+    private final static String[] FROM_COLUMNS = {
+            Build.VERSION.SDK_INT
+                    >= Build.VERSION_CODES.HONEYCOMB ?
+                    ContactsContract.Contacts.DISPLAY_NAME_PRIMARY :
+                    ContactsContract.Contacts.DISPLAY_NAME
+    };
+
+    /*
+     * Defines an array that contains resource ids for the layout views
+     * that get the Cursor column contents. The id is pre-defined in
+     * the Android framework, so it is prefaced with "android.R.id"
+     */
+    private final static int[] TO_IDS = {
+            android.R.id.text1
+    };
+
+    // Define global mutable variables
+    // Define a ListView object
+    ListView mContactsList;
+    // Define variables for the contact the user selects
+    // The contact's _ID value
+    long mContactId;
+    // The contact's LOOKUP_KEY
+    String mContactKey;
+    // A content URI for the selected contact
+    Uri mContactUri;
+    // An adapter that binds the result Cursor to the ListView
+    private SimpleCursorAdapter mCursorAdapter;
+
+
+    //private Button bttst;
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
 
         View view = inflater.inflate(R.layout.contactslist_fragment, container, false);
-        bttst = (Button) view.findViewById(R.id.btnTEST);
+        /*bttst = (Button) view.findViewById(R.id.btnTEST);
 
         bttst.setOnClickListener( (View) -> {
             Toast.makeText(getActivity(), "TEST BTN1", Toast.LENGTH_SHORT).show();
-        } );
+        } );*/
 
         return view;
 
     }
+
+
+
+
+    /*
+    * Set up the SimpleCursorAdapter that binds the results of the search to the ListView.
+    * To get the ListView object that displays the contacts, you need to call
+    * Activity.findViewById() using the parent activity of the Fragment.
+    * Use the Context of the parent activity when you call setAdapter()
+     */
+    public void onActivityCreated(Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+
+        // Initializes the loader
+        getLoaderManager().initLoader(0, null, this);
+
+        // Gets the ListView from the View list of the parent activity
+        mContactsList =
+                (ListView) getActivity().findViewById(R.id.list);
+        // Gets a CursorAdapter
+        mCursorAdapter = new SimpleCursorAdapter(
+                getActivity(),
+                R.layout.contacts_list_item,
+                null,
+                FROM_COLUMNS, TO_IDS,
+                0);
+        // Sets the adapter for the ListView
+        mContactsList.setAdapter(mCursorAdapter);
+
+        // Set the item click listener to be the current fragment.
+        mContactsList.setOnItemClickListener(this);
+
+
+    }
+
+    @Override
+    public Loader<Cursor> onCreateLoader(int loaderId, Bundle args) {
+        /*
+         * Makes search string into pattern and
+         * stores it in the selection array
+         */
+        mSelectionArgs[0] = "%";
+        // Starts the query
+        return new CursorLoader(
+                getActivity(),
+                ContactsContract.Contacts.CONTENT_URI,
+                PROJECTION,
+                SELECTION,
+                mSelectionArgs,
+                null
+        );
+    }
+
+    @Override
+    public void onLoadFinished(Loader<Cursor> loader, Cursor cursor) {
+        // Put the result Cursor in the adapter for the ListView
+        mCursorAdapter.swapCursor(cursor);
+    }
+
+    @Override
+    public void onLoaderReset(Loader<Cursor> loader) {
+        // Delete the reference to the existing Cursor
+        mCursorAdapter.swapCursor(null);
+
+    }
+
+
+
+    @Override
+    public void onItemClick(AdapterView<?> parent, View item, int position, long rowID) {
+        // Get the Cursor
+        Cursor cursor = ((SimpleCursorAdapter) parent.getAdapter()).getCursor();
+        // Move to the selected contact
+        cursor.moveToPosition(position);
+        // Get the _ID value
+        mContactId = cursor.getLong(CONTACT_ID_INDEX);
+        // Get the selected LOOKUP KEY
+        mContactKey = cursor.getString(CONTACT_KEY_INDEX);
+        // Create the contact's content Uri
+        mContactUri = ContactsContract.Contacts.getLookupUri(mContactId, mContactKey);
+        /*
+         * You can use mContactUri as the content URI for retrieving
+         * the details for a contact.
+         */
+    }
+
 }
