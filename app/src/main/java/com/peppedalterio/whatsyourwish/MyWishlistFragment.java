b8b884002f767f91b84ee2bb576f5e3d4c34875a
Giuseppe D'Alterio <dalterio2@gmail.com>
2018-12-23 13:41:47 +0100

    Bozza template con 2 tabs

---
+++ app/src/main/java/com/peppedalterio/whatsyourwish/MyWishlistFragment.java
@@ -0,0 +1,34 @@
+package com.peppedalterio.whatsyourwish;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class MyWishlistFragment extends Fragment {
+
+    private static final String TAG = "WishListFragment";
+
+    private Button bttst;
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+
+        View view = inflater.inflate(R.layout.contactslist_fragment, container, false);
+        bttst = (Button) view.findViewById(R.id.btnTEST);
+
+        bttst.setOnClickListener( (View) -> {
+            Toast.makeText(getActivity(), "WISH BTN1", Toast.LENGTH_SHORT).show();
+        } );
+
+        return view;
+
+    }
+
+}
---
+++ app/src/main/java/com/peppedalterio/whatsyourwish/SectionsPageAdapter.java
@@ -0,0 +1,40 @@
+package com.peppedalterio.whatsyourwish;
+
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+class SectionsPageAdapter extends FragmentPagerAdapter {
+
+    private final List<Fragment> fragmentList = new ArrayList<>();
+    private final List<String> fragmentTitleList = new ArrayList<>();
+
+    public SectionsPageAdapter(FragmentManager fm) {
+        super(fm);
+    }
+
+    public void addFragment(Fragment fragment, String title) {
+        fragmentList.add(fragment);
+        fragmentTitleList.add(title);
+    }
+
+    @Nullable
+    @Override
+    public CharSequence getPageTitle(int position) {
+        return super.getPageTitle(position);
+    }
+
+    @Override
+    public Fragment getItem(int i) {
+        return fragmentList.get(i);
+    }
+
+    @Override
+    public int getCount() {
+        return fragmentList.size();
+    }
+}
